d11e6cc452346ce846a1f292442902f654c63ec6
Andrea Ghidara <Ghidara.andrea.99@gmail.com>
2023-04-14 12:32:09 +0200

    Correzione Classe

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,8 @@ function createTikets() {
     // calcolare costo viaggio
     let priceVoyage = distance * priceKm;
     console.log(priceVoyage);
-
+    
+    // aplicare sconto se necessario
     if ( priceVoyage != NaN) {
         if (age < 18) {
             priceVoyage -= priceVoyage / 100 * 20;
@@ -29,9 +30,9 @@ function createTikets() {
         alert("i dati inseriti non sono validi");
         location.reload();
     }
-    // aplicare sconto se necessario
 }
 
+//=======================================================================================
 
 // Alternative
 
@@ -62,5 +63,61 @@ function createTikets() {
 //     }
 // }
 
+//=======================================================================================
 
+// Correzione Classe
 
+//Differenza tra Number e parseInt
+//parseInt scarta eventuali caratteri non numerici DOPO il numero (ma si spaccherebbe con x123)
+//Number si spacca se ci sono caratteri non numeri PRIMA O DOPO (si spacca con x123 e con 123x) + accetta valori decimali
+
+// let etaUtente = Number( prompt("Inserisci la tua età") );
+// let distance = parseInt( prompt("Inserisci la distanza chilometrica") );
+
+// if( isNaN(distance) ) {
+//     alert("La distanza non è un numero. Impossibile procedere.");
+// } else {
+
+//     let prezzo = distance * 0.21;
+//     let messaggio = "";
+    
+//     console.log("Prezzo di base", prezzo);
+    
+//     if( isNaN(etaUtente) ) {
+//         alert("L'età è errata. Non verranno applicate scontistiche");
+//     } else {
+
+         // if(etaUtente < 18) {
+            // console.log("L'utente è under 18");
+            // prezzo = prezzo - (prezzo / 100) * 20 ;
+            // prezzo -= (prezzo / 100) * 20 ;
+            // prezzo -= (prezzo * 0.2);
+            // prezzo = prezzo * 0.8;
+             // prezzo *= 0.8;
+        
+             // messaggio = `Hai diritto allo sconto 'under 18'.`;
+        
+         // } else if (etaUtente > 65) {
+            // console.log("L'utente è over 65");
+             // prezzo *= 0.55;
+        
+             // messaggio = `Hai diritto allo sconto 'senior'.`;
+        
+        // } 
+        /*
+        else {
+            messaggio = ``;
+        }
+        */
+    // }
+
+    
+    // messaggio = messaggio + ` Il prezzo del biglietto è: ${prezzo} €`;
+    // messaggio += ` Il prezzo del biglietto è: ${ prezzo.toFixed(2) } €`;
+    
+    // document.getElementById("risultato").innerText = messaggio;
+    
+    // console.log("Prezzo finale", prezzo);
+
+// }
+//=======================================================================================
